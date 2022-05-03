0aad959e8b36feef09b0f518c5051a7032f05b86
vdraganovic <vdraganovic@gmail.com>
2022-05-03 13:38:48 +0200

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+
+    #region properties
+
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        #endregion
+
+        #region constructors
+
+        public Joke()
+        {
+
+        }
+
+        #endregion
+    }
+}
